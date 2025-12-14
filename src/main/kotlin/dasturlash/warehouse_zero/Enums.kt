4dc94b22ee8baba547487d135c4e7a0e1ef138a0
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-12-15 00:37:04 +0500

    add: ErrorCode

--- src/main/kotlin/dasturlash/warehouse_zero/Enums.kt
+++ src/main/kotlin/dasturlash/warehouse_zero/Enums.kt
@@ -7,3 +7,8 @@ enum class Status {
 enum class Role {
     EMPLOYEE, ADMIN
 }
+
+enum class ErrorCode(val code: Int, val message: String) {
+    WAREHOUSE_ALREADY_EXISTS(100, "WAREHOUSE_ALREADY_EXISTS"),
+    WAREHOUSE_NOT_FOUND(101, "WAREHOUSE_NOT_FOUND"),
+}

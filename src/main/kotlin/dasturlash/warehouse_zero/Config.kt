45726f1f1dc11eb545b0cf77519244beac498b91
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-12-15 00:36:12 +0500

    add: bean password encoder

--- src/main/kotlin/dasturlash/warehouse_zero/Config.kt
+++ src/main/kotlin/dasturlash/warehouse_zero/Config.kt
@@ -1,2 +1,12 @@
 package dasturlash.warehouse_zero
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Configuration
+class MyConfiguration{
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+}

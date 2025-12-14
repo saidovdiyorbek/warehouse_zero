48eece509c2b0419ae3784592a2689b9ba3428dc
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-12-15 02:39:17 +0500

    add: Employee not found

--- src/main/kotlin/dasturlash/warehouse_zero/Enums.kt
+++ src/main/kotlin/dasturlash/warehouse_zero/Enums.kt
@@ -11,4 +11,5 @@ enum class Role {
 enum class ErrorCode(val code: Int, val message: String) {
     WAREHOUSE_ALREADY_EXISTS(100, "WAREHOUSE_ALREADY_EXISTS"),
     WAREHOUSE_NOT_FOUND(101, "WAREHOUSE_NOT_FOUND"),
+    EMPLOYEE_NOT_FOUND(200, "EMPLOYEE_NOT_FOUND"),
 }

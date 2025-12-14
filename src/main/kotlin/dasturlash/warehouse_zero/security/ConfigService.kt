0ad2eced1e3b84f8509f04e8d409567afc73e519
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-12-15 02:42:02 +0500

    add: filterchain

--- src/main/kotlin/dasturlash/warehouse_zero/security/ConfigService.kt
+++ src/main/kotlin/dasturlash/warehouse_zero/security/ConfigService.kt
@@ -1,2 +1,35 @@
 package dasturlash.warehouse_zero.security
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+class ConfigService(
+    private val jwtAuthFilter: JwtAuthFilter,
+) {
+
+    @Bean
+    fun securityWebFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .cors { it.disable() }
+            .authorizeHttpRequests {
+                it
+                    .requestMatchers("/auth/**").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
+
+        return http.build()
+    }
+}

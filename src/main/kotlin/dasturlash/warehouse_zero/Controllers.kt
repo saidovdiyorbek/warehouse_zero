6f6f05fc90e5b7ca8ef31b8070fada2ef2a7a5c4
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-12-15 02:41:40 +0500

    add: warehouse api

--- src/main/kotlin/dasturlash/warehouse_zero/Controllers.kt
+++ src/main/kotlin/dasturlash/warehouse_zero/Controllers.kt
@@ -1,2 +1,19 @@
 package dasturlash.warehouse_zero
 
+import io.swagger.v3.oas.annotations.Operation
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/warehouses")
+class WarehouseController(
+    private val service: WarehouseService
+) {
+
+    @Operation(summary = "Create a Warehouse")
+    @PostMapping
+    fun create(@Valid @RequestBody create: CreateWarehouseDto) = service.create(create)
+}

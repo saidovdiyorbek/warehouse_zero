c38f010a3002f066fcd14f2909ee814d2e739f60
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-12-15 00:37:51 +0500

    add: BaseRepo and BaseRepoImpl and warehouse employee

--- src/main/kotlin/dasturlash/warehouse_zero/Respositories.kt
+++ src/main/kotlin/dasturlash/warehouse_zero/Respositories.kt
@@ -1,2 +1,53 @@
 package dasturlash.warehouse_zero
 
+import jakarta.persistence.EntityManager
+import jakarta.transaction.Transactional
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.domain.Specification
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.data.jpa.repository.support.JpaEntityInformation
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository
+import org.springframework.data.repository.NoRepositoryBean
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Repository
+
+@NoRepositoryBean
+interface BaseRepository<T : BaseEntity> : JpaRepository<T, Long>, JpaSpecificationExecutor<T> {
+    fun findByIdAndDeletedFalse(id: Long): T?
+    fun trash(id: Long): T?
+    fun trashList(ids: List<Long>): List<T?>
+    fun findAllNotDeleted(): List<T>
+    fun findAllNotDeleted(pageable: Pageable): Page<T>
+}
+
+class BaseRepositoryImpl<T : BaseEntity>(
+    entityInformation: JpaEntityInformation<T, Long>,
+    entityManager: EntityManager
+): SimpleJpaRepository<T, Long>(entityInformation, entityManager), BaseRepository<T> {
+    val isNotDeletedSpecification = Specification<T> { root, _, cb -> cb.equal(root.get<Boolean>("deleted"), true) }
+    override fun findByIdAndDeletedFalse(id: Long): T? = findByIdOrNull(id)?.run { if (deleted) null else this }
+
+    @Transactional
+    override fun trash(id: Long): T? = findByIdOrNull(id)?.run {
+        deleted = true
+        save(this)
+    }
+
+    override fun trashList(ids: List<Long>): List<T?> = ids.map { trash(it) }
+
+    override fun findAllNotDeleted(): List<T> = findAll(isNotDeletedSpecification)
+
+    override fun findAllNotDeleted(pageable: Pageable): Page<T> = findAll(isNotDeletedSpecification, pageable)
+
+}
+
+@Repository
+interface WarehouseRepository : BaseRepository<Warehouse>{
+    fun existsWarehouseByName(name: String): Boolean
+}
+
+interface EmployeeRepository : BaseRepository<Employee>{
+    fun findByPhoneNumberAndDeletedFalse(phoneNumber: String): Employee
+}

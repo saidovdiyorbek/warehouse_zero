726dfe470a7815f5794612728f37662ca315a076
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-12-15 02:36:37 +0500

    add: custom user service and warehouse service

--- src/main/kotlin/dasturlash/warehouse_zero/Services.kt
+++ src/main/kotlin/dasturlash/warehouse_zero/Services.kt
@@ -1,2 +1,46 @@
 package dasturlash.warehouse_zero
 
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Service
+
+//Warehouse Service
+interface WarehouseService {
+    fun create(create: CreateWarehouseDto)
+}
+
+@Service
+class WarehouseServiceImpl(
+    private val repository: WarehouseRepository
+) : WarehouseService {
+    override fun create(create: CreateWarehouseDto) {
+        repository.existsWarehouseByName(create.name).takeIf { it }?.let {
+            repository.save(Warehouse(
+                name = create.name,
+            ))
+            return
+        }
+        throw WarehouseAlreadyExistsException()
+    }
+}
+//Warehouse Service
+
+//Custom User detail service
+@Service
+class CustomUserService(
+    private val repository: EmployeeRepository
+) : UserDetailsService {
+    override fun loadUserByUsername(phoneNumber: String): UserDetails {
+        return repository.findByPhoneNumberAndDeletedFalse(phoneNumber)?.let {
+            UserDetailsResponse(
+                id = it.id!!,
+                phoneNumber = it.phoneNumber,
+                firstName = it.firstName,
+                lastName = it.lastName,
+                role = it.role,
+                mypassword = it.password
+            )
+        } ?: throw EmployeeNotFoundException()
+    }
+}
+//Custom User detail service

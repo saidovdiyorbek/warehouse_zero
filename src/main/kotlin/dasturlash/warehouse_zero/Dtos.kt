6d794f47b69ab71d5d0a94f86428cda800d92d52
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-12-15 00:36:48 +0500

    add: warehouse create body and custom user details

--- src/main/kotlin/dasturlash/warehouse_zero/Dtos.kt
+++ src/main/kotlin/dasturlash/warehouse_zero/Dtos.kt
@@ -1,2 +1,41 @@
 package dasturlash.warehouse_zero
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+data class BaseMessage(val code: Int?, val message: String? = null){
+    companion object{
+        var OK = BaseMessage(code = 0, message = "OK")
+    }
+}
+
+data class CreateWarehouseDto(
+    @field:NotBlank("Warehouse name not be blank")
+    @field:Size(min = 4, max = 100, message = "Warehouse name size between 4 and 100 characters.")
+    var name: String,
+)
+
+data class UserDetailsResponse(
+    val id: Long,
+    val phoneNumber: String,
+    val firstName: String,
+    val lastName: String?,
+    val role: Role,
+    val password: String,
+) : UserDetails{
+    override fun getAuthorities(): Collection<GrantedAuthority?> {
+        return listOf(SimpleGrantedAuthority(role.name))
+    }
+
+    override fun getPassword(): String {
+        return password
+    }
+
+    override fun getUsername(): String {
+        return phoneNumber
+    }
+
+}

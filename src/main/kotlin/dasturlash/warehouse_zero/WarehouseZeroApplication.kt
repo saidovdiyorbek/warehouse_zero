4c17f85e2b801af74006fd75cfbfccb6041ed939
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-12-14 17:25:28 +0500

    Initial commit

---
+++ src/main/kotlin/dasturlash/warehouse_zero/WarehouseZeroApplication.kt
@@ -0,0 +1,11 @@
+package dasturlash.warehouse_zero
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WarehouseZeroApplication
+
+fun main(args: Array<String>) {
+    runApplication<WarehouseZeroApplication>(*args)
+}
---
+++ src/test/kotlin/dasturlash/warehouse_zero/WarehouseZeroApplicationTests.kt
@@ -0,0 +1,13 @@
+package dasturlash.warehouse_zero
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WarehouseZeroApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

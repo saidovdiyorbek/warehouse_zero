641b9f601accf4aa323f039f2ee52089633c954e
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-12-15 00:38:27 +0500

    add: annotation @EnableJpaRepositories

--- src/main/kotlin/dasturlash/warehouse_zero/WarehouseZeroApplication.kt
+++ src/main/kotlin/dasturlash/warehouse_zero/WarehouseZeroApplication.kt
@@ -3,7 +3,9 @@ package dasturlash.warehouse_zero
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
+@EnableJpaRepositories(repositoryBaseClass = BaseRepositoryImpl::class)
 @SpringBootApplication
 @EnableJpaAuditing
 class WarehouseZeroApplication

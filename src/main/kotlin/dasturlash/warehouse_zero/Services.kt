4ea805a67cfe0081e6b83007dc4a159e09d76dd3
HP <saidovdiyer777@gmail.com>
2025-12-15 15:18:31 +0500

    add: warehouse CRUD service

--- src/main/kotlin/dasturlash/warehouse_zero/Services.kt
+++ src/main/kotlin/dasturlash/warehouse_zero/Services.kt
@@ -1,12 +1,17 @@
 package dasturlash.warehouse_zero
 
+import dasturlash.warehouse_zero.security.JwtService
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 //Warehouse Service
 interface WarehouseService {
     fun create(create: CreateWarehouseDto)
+    fun getOne(id: Long): WarehouseResponse
+    fun update(id: Long, update: WarehouseUpdateRequest)
+    fun delete(id: Long)
 }
 
 @Service
@@ -14,13 +19,34 @@ class WarehouseServiceImpl(
     private val repository: WarehouseRepository
 ) : WarehouseService {
     override fun create(create: CreateWarehouseDto) {
-        repository.existsWarehouseByName(create.name).takeIf { it }?.let {
-            repository.save(Warehouse(
-                name = create.name,
-            ))
+        repository.existsWarehouseByName(create.name)?.let {
+            throw WarehouseAlreadyExistsException()
+        }
+        repository.save(Warehouse(name = create.name,))
+    }
+
+    override fun getOne(id: Long): WarehouseResponse {
+        val warehouse = repository.findByIdAndDeletedFalse(id)?.let { warehouse ->
+            return WarehouseResponse(
+                warehouse.id!!,
+                warehouse.name,
+                warehouse.createdBy
+            )
+        }
+        throw WarehouseNotFoundException()
+    }
+
+    override fun update(id: Long, update: WarehouseUpdateRequest) {
+        repository.findByIdAndDeletedFalse(id)?.let { warehouse ->
+            warehouse.name = update.name
+            repository.save(warehouse)
             return
         }
-        throw WarehouseAlreadyExistsException()
+        throw WarehouseNotFoundException()
+    }
+
+    override fun delete(id: Long) {
+        repository.trash(id) ?: throw WarehouseNotFoundException()
     }
 }
 //Warehouse Service
@@ -44,3 +70,28 @@ class CustomUserService(
     }
 }
 //Custom User detail service
+
+//Employee Service
+interface EmployeeService {
+    fun login(request: LoginRequest): JwtResponse
+}
+
+@Service
+class EmployeeServiceImpl(
+    private val repository: EmployeeRepository,
+    private val passwordEncoder: PasswordEncoder,
+    private val jwtService: JwtService,
+) : EmployeeService {
+    override fun login(request: LoginRequest): JwtResponse {
+       val user = repository.findByPhoneNumberAndDeletedFalse(request.phoneNumber)
+        ?: throw EmployeeNotFoundException()
+
+       if (!passwordEncoder.matches(request.password, user.password))
+           throw InvalidPasswordException()
+
+       val token = jwtService.generateToken(user.phoneNumber, user.role.name)
+
+       return JwtResponse(token)
+    }
+}
+//Employee Service

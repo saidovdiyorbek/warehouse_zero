df40c573a6a2c6911e3dfe089f76accf63585fc9
HP <saidovdiyer777@gmail.com>
2025-12-15 15:15:36 +0500

    add: role cons

--- src/main/kotlin/dasturlash/warehouse_zero/Enums.kt
+++ src/main/kotlin/dasturlash/warehouse_zero/Enums.kt
@@ -5,11 +5,12 @@ enum class Status {
 }
 
 enum class Role {
-    EMPLOYEE, ADMIN
+    ROLE_EMPLOYEE, ROLE_ADMIN
 }
 
 enum class ErrorCode(val code: Int, val message: String) {
     WAREHOUSE_ALREADY_EXISTS(100, "WAREHOUSE_ALREADY_EXISTS"),
     WAREHOUSE_NOT_FOUND(101, "WAREHOUSE_NOT_FOUND"),
     EMPLOYEE_NOT_FOUND(200, "EMPLOYEE_NOT_FOUND"),
+    INVALID_PASSWORD(300, "INVALID_PASSWORD"),
 }

753fbd99b771178b5de6ec32095f748a9750566f
HP <saidovdiyer777@gmail.com>
2025-12-15 15:17:53 +0500

    add: login req, warehouse res, ware update req

--- src/main/kotlin/dasturlash/warehouse_zero/Dtos.kt
+++ src/main/kotlin/dasturlash/warehouse_zero/Dtos.kt
@@ -1,6 +1,7 @@
 package dasturlash.warehouse_zero
 
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.Size
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
@@ -13,8 +14,8 @@ data class BaseMessage(val code: Int?, val message: String? = null){
 }
 
 data class CreateWarehouseDto(
-    @field:NotBlank("Warehouse name not be blank")
-    @field:Size(min = 4, max = 100, message = "Warehouse name size between 4 and 100 characters.")
+    @field:NotBlank("name.not.blank")
+    @field:Size(min = 4, max = 100, message = "min4.max100")
     var name: String,
 )
 
@@ -39,3 +40,27 @@ data class UserDetailsResponse(
     }
 
 }
+
+data class LoginRequest(
+    @field:NotBlank(message = "Phone number is required")
+    @field:Pattern(
+        regexp = "^\\+998(90|91|93|94|95|97|98|99|33|50|88)\\d{7}$",
+        message = "phone.number.regex"
+    )
+    val phoneNumber: String,
+    @field:NotBlank("password.required") val password: String,
+)
+
+data class JwtResponse(val token: String)
+
+data class WarehouseResponse(
+    val id: Long,
+    val name: String,
+    val createdBy: String?,
+)
+
+data class WarehouseUpdateRequest(
+    @field:NotBlank
+    @field:Size(min = 4, max = 100, message = "Warehouse name size between 4 and 100 characters.")
+    val name: String
+)

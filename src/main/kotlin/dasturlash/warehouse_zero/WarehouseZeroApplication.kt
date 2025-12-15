a24d96c4635e72b33223a5bf93d3ca6dbc54fbe0
HP <saidovdiyer777@gmail.com>
2025-12-15 15:18:47 +0500

    add: Security schema

--- src/main/kotlin/dasturlash/warehouse_zero/WarehouseZeroApplication.kt
+++ src/main/kotlin/dasturlash/warehouse_zero/WarehouseZeroApplication.kt
@@ -1,11 +1,20 @@
 package dasturlash.warehouse_zero
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.security.SecurityScheme
+import io.swagger.v3.oas.annotations.security.SecuritySchemes
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @EnableJpaRepositories(repositoryBaseClass = BaseRepositoryImpl::class)
+@SecurityScheme(
+    name = "Bearer Authentication",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer"
+)
 @SpringBootApplication
 @EnableJpaAuditing
 class WarehouseZeroApplication

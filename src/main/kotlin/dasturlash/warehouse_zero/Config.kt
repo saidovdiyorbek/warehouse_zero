49c1544ddb2fe7b1d910024b4df3241509a4bd65
HP <saidovdiyer777@gmail.com>
2025-12-15 15:55:31 +0500

    add: Security schema changed from annotation to class config

--- src/main/kotlin/dasturlash/warehouse_zero/Config.kt
+++ src/main/kotlin/dasturlash/warehouse_zero/Config.kt
@@ -2,6 +2,7 @@ package dasturlash.warehouse_zero
 
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springdoc.core.customizers.OperationCustomizer
 import org.springframework.context.MessageSource
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -17,6 +18,16 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver
 import org.springframework.web.servlet.support.RequestContextUtils
 import java.util.Locale
 import java.util.Optional
+import org.springframework.web.method.HandlerMethod
+import io.swagger.v3.oas.models.Operation
+import io.swagger.v3.oas.models.parameters.Parameter
+import io.swagger.v3.oas.annotations.enums.ParameterIn
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.media.StringSchema
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 
 @Configuration
 class WebMvcConfig : WebMvcConfigurer {
@@ -68,4 +79,43 @@ class AuditConfig{
             )
         }
     }
+}
+
+@Configuration
+class OpenApiConfig {
+
+    @Bean
+    fun openAPI(): OpenAPI {
+        return OpenAPI()
+            .components(
+                Components()
+                    .addSecuritySchemes(
+                        "bearerAuth",
+                        SecurityScheme()
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme("bearer")
+                            .bearerFormat("JWT")
+                    )
+            )
+            .addSecurityItem(
+                SecurityRequirement().addList("bearerAuth")
+            )
+    }
+
+    @Bean
+    fun globalHeaderCustomizer() : OperationCustomizer{
+        return OperationCustomizer {operation, _ ->
+
+            val localeHeader = Parameter()
+                .`in`("header")
+                .schema(StringSchema())
+                .name("Accept-Language")
+                .description("Language (uz, ru, en)")
+                .required(false)
+
+            operation.addParametersItem(localeHeader)
+            operation
+        }
+    }
+
 }

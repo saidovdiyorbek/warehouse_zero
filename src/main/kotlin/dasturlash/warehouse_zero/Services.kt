8da7e3dcf9090c892371f9fb4308807e995028cc
HP <saidovdiyer777@gmail.com>
2025-12-16 16:45:25 +0500

    add: Attach upload servie

--- src/main/kotlin/dasturlash/warehouse_zero/Services.kt
+++ src/main/kotlin/dasturlash/warehouse_zero/Services.kt
@@ -1,10 +1,21 @@
 package dasturlash.warehouse_zero
 
 import dasturlash.warehouse_zero.security.JwtService
+import io.jsonwebtoken.io.IOException
+import jakarta.transaction.Transactional
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.properties.source.ConfigurationPropertySources.attach
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.util.Calendar
+import java.util.Objects.hash
+import kotlin.io.path.Path
 
 //Warehouse Service
 interface WarehouseService {
@@ -157,3 +168,119 @@ class CategoryServiceImpl(
 
 }
 //Category Service
+
+//Attach Service
+interface AttachService {
+    fun upload(productId: Long, file: MultipartFile): AttachUrl
+    fun generateDataBaseFolder(): String
+    fun getExtension(fileName: String?): String
+    fun createAttachEntity(file: MultipartFile, hash: String, extension: String, pathFolder: String, product: Product): Attach
+    fun generateHash(): String
+    fun openUrl(hash: String): String
+    fun isExist(hash: String): Boolean
+    fun saveAttach(file: MultipartFile, pathFolder: String, hash: String, extension: String): String
+}
+
+@Service
+class AttachServiceImpl(
+    @Value("\${attach.upload.folder}")private val folderName: String,
+    @Value("\${attach.url}")private val attachUrl: String,
+
+    private val productRepository: ProductRepository,
+    private val repository: AttachRepository
+) : AttachService {
+
+    @Transactional
+    override fun upload(productId: Long, file: MultipartFile): AttachUrl {
+        val findProduct = productRepository.findByIdAndDeletedFalse(productId)
+            ?: throw ProductNotFoundException()
+
+        val pathFolder: String = generateDataBaseFolder()
+        val extension: String = getExtension(file.originalFilename)
+        val hash: String = generateHash()
+        val fullFilePath = saveAttach(file, pathFolder, hash, extension)
+
+        val attach = createAttachEntity(file, hash, extension, pathFolder, findProduct!!)
+
+        return AttachUrl(attach.id!!, openUrl(hash) )
+    }
+
+
+
+     override fun generateDataBaseFolder(): String {
+        val cal: Calendar = Calendar.getInstance()
+        val folder: String = "${cal.get(Calendar.YEAR)}/" +
+                "${cal.get(Calendar.MONTH) + 1}/" +
+                "${cal.get(Calendar.DATE)}"
+        return folder
+    }
+
+     override fun getExtension(fileName: String?): String {
+        val lastIndex = fileName?.lastIndexOf(".")
+        return fileName!!.substring(lastIndex!!.plus(1))
+    }
+
+    override fun createAttachEntity(
+        file: MultipartFile,
+        hash: String,
+        extension: String,
+        pathFolder: String,
+        product: Product
+    ): Attach {
+        val attach = Attach(
+            originName = file.originalFilename,
+            size = file.size,
+            type = file.contentType,
+            path = pathFolder,
+            hash = hash,
+            product = product,
+        )
+
+        return attach
+    }
+
+    override fun generateHash(): String {
+        val charPool : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+        val randomString =  (1..10)
+            .map {i -> kotlin.random.Random.nextInt(0, charPool.size).let { charPool[it] }}
+            .joinToString("")
+        return randomString
+    }
+
+    override fun openUrl(hash: String): String {
+          if (isExist(hash)){
+              return "$attachUrl/open/$hash"
+          }
+        return " "
+    }
+
+    override fun isExist(hash: String): Boolean {
+        return repository.existsByHashAndDeletedFalse(hash)
+    }
+
+    override fun saveAttach(
+        file: MultipartFile,
+        pathFolder: String,
+        hash: String,
+        extension: String
+    ): String {
+        val path = Path(folderName)
+        try {
+            if (!Files.exists(path)){
+                Files.createDirectories(path)
+            }
+
+            val fullFileName = "$hash.$extension"
+            val fullPath: Path = Paths.get("$folderName/$pathFolder/$fullFileName")
+            Files.createDirectories(fullPath.parent)
+            Files.write(fullPath, file.bytes)
+
+            return fullPath.toString()
+
+        }catch (e: IOException){
+            throw RuntimeException("Failed to create file", e)
+        }
+    }
+
+}
+//Attach Service

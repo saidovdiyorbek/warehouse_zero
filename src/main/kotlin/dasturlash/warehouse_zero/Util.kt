e67a0ba539accce203fc746fb8ff4f0bf6868999
HP <saidovdiyer777@gmail.com>
2025-12-17 05:18:42 +0500

    add: util generateHash fun

---
+++ src/main/kotlin/dasturlash/warehouse_zero/Util.kt
@@ -0,0 +1,15 @@
+﻿package dasturlash.warehouse_zero
+
+import org.springframework.stereotype.Component
+
+@Component
+class GenerateHash(){
+
+    fun generateHash(): String {
+        val charPool : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+        val randomString =  (1..10)
+            .map {i -> kotlin.random.Random.nextInt(0, charPool.size).let { charPool[it] }}
+            .joinToString("")
+        return randomString
+    }
+}

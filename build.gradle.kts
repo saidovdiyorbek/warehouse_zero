c8b0c2ab636e94627e85b7b820faf42bb1a5e70a
HP <saidovdiyer777@gmail.com>
2025-12-17 17:07:28 +0500

    add: dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
     implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
     implementation("org.liquibase:liquibase-core")
+    implementation("org.telegram:telegrambots-spring-boot-starter:6.9.7.1")
+
 }
 
 kotlin {
